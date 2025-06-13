c70fa2fd64a3470723b3c12adf5a389961082601
Ehtisham Afzal <khaj3172@gmail.com>
2025-06-13 17:34:57 +0500

    Changes to be committed: 	modified:   dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -7,3 +7,20 @@ students = {
 
 for student in students:
     print(student, students[student], sep=", ")
+
+
+def list_of_dictionaries():
+    students = [
+        {"name": "Hermione", "house": "Graffindore", "patrounus": "otter"},
+        {"name": "Harry", "house": "Stag", "patrounus": "otter"},
+        {"name": "Rone", "house": "Jack russel terrior", "patrounus": "otter"},
+        {"name": "Draco", "house": "Slytherin", "patrounus": None},
+    ]
+
+    for student in students:
+        print(
+            f"the name of student is {student['name']} living on {student['house']} and has {student['patrounus']} patrounus"
+        )
+
+
+list_of_dictionaries()
