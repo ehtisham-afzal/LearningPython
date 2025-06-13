7e18218d0439789910f1431582a1e0ae0af39208
Ehtisham Afzal <khaj3172@gmail.com>
2025-06-13 16:16:28 +0500

    Changes to be committed: 	modified:   dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+students = {
+    "Hermione": "Graffindore",
+    "Harry": "Graffindore",
+    "Draco": "Graffindore",
+    "Ron": "Slythenren",
+}
+
+for student in students:
+    print(student, students[student], sep=", ")
