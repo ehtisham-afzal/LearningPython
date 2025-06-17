cec47eb269c725e7e9b4544e96fece5f1b8fc9be
Ehtisham Afzal <khaj3172@gmail.com>
2025-06-17 10:48:19 +0500

    Changes to be committed: 	modified:   mario.py

--- mario.py
+++ mario.py
@@ -1,20 +1,57 @@
 def main():
-    print_square(3)
+    height = int(input("Height: "))
+    print_pyramid(height)
 
 
-def print_square(size):
-    for _ in range(size):
-        print_row(size)
-
-
-def print_row(blocks):
-    # first method
-    for _ in range(blocks):
-        print("#", end="")
-    print()
-
-    # second method
-    # print("#" * blocks)
+def print_pyramid(height):
+    for row in range(height):
+        print("❎" * (row + 1))
 
 
 main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def square():
+#     height = int(input("Height: "))
+#     print_square(height)
+
+
+# def print_square(size):
+#     for _ in range(size):
+#         print_row(size)
+
+
+# def print_row(blocks):
+#     # first method
+#     for _ in range(blocks):
+#         print("❎", end="")
+#     print()
+
+#     # second method
+#     # print("❎" * blocks)
+
+
+# square()
